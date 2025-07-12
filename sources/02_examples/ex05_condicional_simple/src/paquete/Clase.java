f22d7d0eab47c462fc279ab5ea38d2dbfec96e5d
ProfeAlbeiro <profealbeiro2020@gmail.com>
2025-07-12 09:45:38 -0500

    Creando tutorial java

---
+++ sources/02_examples/ex05_condicional_simple/src/paquete/Clase.java
@@ -0,0 +1,29 @@
+package paquete;
+
+public class Clase {
+
+    public static void main(String[] args) {
+        // Objetivo del Algoritmo: Analizar el Objetivo / Problema como un Sistema [Entradas, Proceso y Salidas]
+	System.out.println("Número Mayor");
+        
+	// Declarar variables, constantes y/o arreglos.
+	int num01, num02;
+        
+	// Dimensionar arreglos.
+	
+	// Iniciar variables, constantes y/o arreglos
+	num01 = 10;
+	num02 = 5;
+        
+	// Entrada de Datos: Pueden ser solicitados o no
+	
+	// Proceso: Determinar los requerimientos y validaciones. Generalmente,	
+	//          se usan las estructuras de control: Secuencial, condicional, repetición.
+	if (num01 > num02) {
+            // Salida de Datos. Resultado(s).	        
+            System.out.println("El Número Uno es mayor al Número 2");
+        }
+        
+    }
+    
+}
