eff8eeb28cba7caf47b1314436dda6be8fecd5e4
ProfeAlbeiro <profealbeiro2020@gmail.com>
2025-07-18 12:10:38 -0500

    proyecto poo_java.txt

--- sources/04_projects/gestor_usuarios_mvc/src/controllers/UserController.java
+++ sources/04_projects/gestor_usuarios_mvc/src/controllers/UserController.java
@@ -5,5 +5,32 @@ import models.User;
 import java.util.ArrayList;
 
 public class UserController {
+    private ArrayList<Rol> roles = new ArrayList<>();
     private ArrayList<User> users = new ArrayList<>();
+    
+    public void addRol(Rol rol){
+        roles.add(rol);
+    }
+    
+    public void addUser(User user){
+        users.add(user);
+    }
+    
+    public ArrayList<User> getUsers(){
+        return users;
+    }
+    
+    public ArrayList<Rol> getRoles(){
+        return roles;
+    }
+    
+    public User authenticateUser(String email, String password){
+        for (User u : users) {
+            if (u.getUserEmail().equalsIgnoreCase(email) && u.getUserPass().equals(password) ) {
+                return u;
+            }
+        }
+        return null;
+    }    
+    
 }
