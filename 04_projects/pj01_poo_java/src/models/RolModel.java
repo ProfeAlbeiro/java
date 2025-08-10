7a83d2a31c92337b158d1bbbd74cdf8434f70b2b
ProfeAlbeiro <profealbeiro2020@gmail.com>
2025-08-10 08:53:07 -0500

    proyecto poo_java.txt

--- 04_projects/pj01_poo_java/src/models/RolModel.java
+++ 04_projects/pj01_poo_java/src/models/RolModel.java
@@ -1,13 +1,32 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package models;
 
-/**
- *
- * @author ADMIN
- */
 public class RolModel {
+    
+    private int rolId;
+    private int rolName;
+
+    public RolModel() {
+    }
+    
+    public RolModel(int rolId, int rolName) {
+        this.rolId = rolId;
+        this.rolName = rolName;
+    }
+
+    public int getRolId() {
+        return rolId;
+    }
+
+    public void setRolId(int rolId) {
+        this.rolId = rolId;
+    }
+
+    public int getRolName() {
+        return rolName;
+    }
+
+    public void setRolName(int rolName) {
+        this.rolName = rolName;
+    }
     
 }
