7247f093cd6feae36f3089fa20e0ba8bc6b57064
ProfeAlbeiro <profealbeiro2020@gmail.com>
2025-08-11 22:00:56 -0500

    proyecto poo_java.txt

--- 04_projects/pj01_poo_java/src/views/modules/roles/RolesView.java
+++ 04_projects/pj01_poo_java/src/views/modules/roles/RolesView.java
@@ -1,13 +1,63 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package views.modules.roles;
 
-/**
- *
- * @author ADMIN
- */
+import java.util.Scanner;
+
 public class RolesView {
+    
+    private int menu;
+    private Scanner sc = new Scanner(System.in);
+
+    public RolesView() {
+        
+        do {
+            
+            System.out.println("\n|-------------------------------------------------------------------|");
+            System.out.println("|------------------------ GESTIÓN DE ROLES -------------------------|");
+            System.out.println("|-------------------------------------------------------------------|");
+            System.out.println("    1. Registrar Rol");
+            System.out.println("    2. Listar Roles");
+            System.out.println("    3. Actualizar Rol");
+            System.out.println("    4. Eliminar Rol");
+            System.out.println("    0. Cerrar Sesión");
+            System.out.println("|-------------------------------------------------------------------|");
+            System.out.print("|----- Seleccione una opción : ");
+            menu = sc.nextInt();            
+            System.out.println("|-------------------------------------------------------------------|");
+            
+            switch (menu) {
+                
+                case 1:
+                    System.out.println("\n|-------------------------------------------------------------------|");
+                    System.out.println("|------------------------- REGISTRAR ROL ---------------------------|");
+                    System.out.println("|-------------------------------------------------------------------|");
+                    break;
+                case 2:
+                    System.out.println("\n|-------------------------------------------------------------------|");
+                    System.out.println("|------------------------ CONSULTAR ROLES --------------------------|");
+                    System.out.println("|-------------------------------------------------------------------|");
+                    break;
+                case 3:
+                    System.out.println("\n|-------------------------------------------------------------------|");
+                    System.out.println("|------------------------- ACTUALIZAR ROL --------------------------|");
+                    System.out.println("|-------------------------------------------------------------------|");
+                    break;
+                case 4:
+                    System.out.println("\n|-------------------------------------------------------------------|");
+                    System.out.println("|-------------------------- ELIMINAR ROL ---------------------------|");
+                    System.out.println("|-------------------------------------------------------------------|");
+                    break;
+                case 0:
+                    System.out.println("|----- ¡Sesión Cerrada! ...");
+                    System.out.println("|-------------------------------------------------------------------|");        
+                    break;
+                default:
+                    System.out.println("|----- ¡Opción No Válida!");
+                    System.out.println("|-------------------------------------------------------------------|");
+                    
+            }
+            
+        } while (menu != 0);
+        
+    }
     
 }
