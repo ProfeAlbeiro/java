4195f49b5dc42191e137784d5566afc751c44689
ProfeAlbeiro <profealbeiro2020@gmail.com>
2025-08-29 23:31:48 -0500

    proyecto poo_java.txt

--- 04_projects/pj01_poo_java/src/controllers/RolesController.java
+++ 04_projects/pj01_poo_java/src/controllers/RolesController.java
@@ -1,15 +1,49 @@
 package controllers;
 
+import models.RolModel;
 import views.modules.roles.RolesMenuView;
+import java.util.ArrayList;
 
 public class RolesController {
     
     private RolesMenuView rolesMenuView;
-
-    public void rolesMenu() {
-        
-        rolesMenuView = new RolesMenuView();
-        
+    private ArrayList<RolModel> roles = new ArrayList<>();
+    
+    public RolesController() {
+    }
+    
+    public void rolesMenu() {        
+        rolesMenuView = new RolesMenuView();        
+    }
+    
+    public void addRol(RolModel rol){
+        roles.add(rol);
+    }    
+    
+    public ArrayList<RolModel> getRoles(){
+        return roles;
+    }
+    
+    public RolModel searchRolById(int id){
+        for (RolModel rol : roles) {
+            if (rol.getRolId() == id) {
+                return rol;
+            }
+        }
+        return null;
+    }    
+    
+    public boolean updateRol(int id, String newName){
+        RolModel rol = searchRolById(id);
+        if (rol != null) {
+            rol.setRolName(newName);
+            return true;
+        }
+        return false;
+    }
+    
+    public boolean deleteRol(int id){
+        return roles.removeIf(r -> r.getRolId() == id);
     }
     
 }
--- 04_projects/pj01_poo_java/src/views/modules/roles/RolesFormView.java
+++ 04_projects/pj01_poo_java/src/views/modules/roles/RolesFormView.java
@@ -1,5 +1,19 @@
 package views.modules.roles;
 
+import java.util.Scanner;
+
 public class RolesFormView {
+
+    Scanner sc = new Scanner(System.in);    
+
+    public int getRolId() {
+        System.out.print("|----- Id:  ");
+        return sc.nextInt();
+    }
+    
+    public String getRolName() {
+        System.out.print("|----- Rol: ");
+        return sc.next();
+    }
     
 }
