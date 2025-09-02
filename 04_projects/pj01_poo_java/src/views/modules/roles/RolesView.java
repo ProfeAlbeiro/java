ce1f8d245473e48ee89b96b09ff6500c32c1e0b0
ProfeAlbeiro <profealbeiro2020@gmail.com>
2025-09-02 15:34:27 -0500

    proyecto poo_java.txt

--- 04_projects/pj01_poo_java/src/views/modules/roles/RolesView.java
+++ 04_projects/pj01_poo_java/src/views/modules/roles/RolesView.java
@@ -1,10 +1,16 @@
 package views.modules.roles;
 
+import models.RolModel;
+import controllers.RolesController;
 import java.util.Scanner;
 
 public class RolesView {
     
-    private int menu;
+    private int menu, rolId;
+    private String rolName;
+    private RolesFormView rolesFormView = new RolesFormView();
+    private RolesController roles = new RolesController();
+    private RolModel rol = new RolModel();
     private Scanner sc = new Scanner(System.in);
 
     public void rolesMenuView() {
@@ -27,36 +33,74 @@ public class RolesView {
             
             switch (menu) {
 
-                case 1:
+                case 1 -> {
                     System.out.println("\n|-------------------------------------------------------------------|");
                     System.out.println("|-------------------------- REGISTRAR ROL --------------------------|");
+                    System.out.println("|-------------------------------------------------------------------|");                    
+                    rolId = roles.getRoles().size() + 1;
+                    rolName = rolesFormView.getRolName();
+                    roles.addRol(new RolModel(rolId, rolName));
                     System.out.println("|-------------------------------------------------------------------|");
-                    break;
-                case 2:
+                    System.out.println("|----- ¡Rol creado!");
+                    System.out.println("|-------------------------------------------------------------------|");
+                }
+                case 2 -> {
                     System.out.println("\n|-------------------------------------------------------------------|");
                     System.out.println("|-------------------------- LISTAR ROLES ---------------------------|");
                     System.out.println("|-------------------------------------------------------------------|");
-                    break;
-                case 3:
+                    for (RolModel getRol : roles.getRoles()) {
+                        System.out.println("|-- " + getRol);
+                    }
+                    System.out.println("|-------------------------------------------------------------------|");
+                }
+                case 3 -> {
                     System.out.println("\n|-------------------------------------------------------------------|");
                     System.out.println("|-------------------------- CONSULTAR ROL --------------------------|");
                     System.out.println("|-------------------------------------------------------------------|");
-                    break;
-                case 4:
+                    rolId = rolesFormView.getRolId();                    
+                    System.out.println("|-------------------------------------------------------------------|");
+                    if (roles.searchRolById(rolId) != null) {
+                        rol = roles.searchRolById(rolId);
+                        System.out.println("|-----      " + rol);                                            
+                    } else {
+                        System.out.println("|---- ¡Rol No Encontrado!");
+                    }                    
+                    System.out.println("|-------------------------------------------------------------------|");
+                }
+                case 4 -> {
                     System.out.println("\n|-------------------------------------------------------------------|");
                     System.out.println("|-------------------------- ACTUALIZAR ROL -------------------------|");
                     System.out.println("|-------------------------------------------------------------------|");
-                    break;
-                case 5:
+                    rolId = rolesFormView.getRolId();
+                    if (roles.searchRolById(rolId) != null) {
+                        rolName = rolesFormView.getRolName();
+                        System.out.println("|-------------------------------------------------------------------|");
+                        roles.updateRol(rolId, rolName);
+                        System.out.println("|---- ¡Rol Actualizado!");
+                    } else {
+                        System.out.println("|-------------------------------------------------------------------|");
+                        System.out.println("|---- ¡Rol No Encontrado!");
+                    }
+                    System.out.println("|-------------------------------------------------------------------|");
+                }
+                case 5 -> {
                     System.out.println("\n|-------------------------------------------------------------------|");
                     System.out.println("|-------------------------- ELIMINAR ROL ---------------------------|");
                     System.out.println("|-------------------------------------------------------------------|");
-                    break;
-                case 0:
-                    break;
-                default:
+                    rolId = rolesFormView.getRolId();
+                    System.out.println("|-------------------------------------------------------------------|");
+                    if (roles.deleteRol(rolId)) {
+                        System.out.println("|---- ¡Rol Eliminado!");
+                    } else {
+                        System.out.println("|---- ¡Rol No Encontrado!");
+                    }
+                    System.out.println("|-------------------------------------------------------------------|");
+                }
+                case 0 -> {}
+                default -> {
                     System.out.println("|----- ¡Opción No Válida!");
                     System.out.println("|-------------------------------------------------------------------|");
+                }
                     
             }
             
