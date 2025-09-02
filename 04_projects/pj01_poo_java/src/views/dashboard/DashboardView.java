2fe22e6a59a5da0f26d13731984a5021420eddea
ProfeAlbeiro <profealbeiro2020@gmail.com>
2025-09-02 14:22:50 -0500

    proyecto poo_java.txt

--- 04_projects/pj01_poo_java/src/views/dashboard/DashboardView.java
+++ 04_projects/pj01_poo_java/src/views/dashboard/DashboardView.java
@@ -1,10 +1,14 @@
 package views.dashboard;
 
+import views.modules.roles.RolesView;
+import views.modules.users.UsersView;
 import java.util.Scanner;
 
 public class DashboardView {
     
     private int menu;
+    private RolesView rolesView;
+    private UsersView usersView;
     private Scanner sc = new Scanner(System.in);
 
     public void dashboardMenuView() {
@@ -24,23 +28,22 @@ public class DashboardView {
             
             switch (menu) {
                 
-                case 1:
-                    System.out.println("\n|-------------------------------------------------------------------|");
-                    System.out.println("|------------------------ GESTIÓN DE ROLES -------------------------|");
-                    System.out.println("|-------------------------------------------------------------------|");
-                    break;
-                case 2:
-                    System.out.println("\n|-------------------------------------------------------------------|");
-                    System.out.println("|---------------------- GESTIÓN DE USUARIOS ------------------------|");
-                    System.out.println("|-------------------------------------------------------------------|");
-                    break;
-                case 0:
+                case 1 -> {
+                    rolesView = new RolesView();
+                    rolesView.rolesMenuView();
+                }
+                case 2 -> {
+                    usersView = new UsersView();
+                    usersView.usersMenuView();
+                }
+                case 0 -> {
                     System.out.println("|----- ¡Sesión Cerrada! ...");
-                    System.out.println("|-------------------------------------------------------------------|");        
-                    break;
-                default:
+                    System.out.println("|-------------------------------------------------------------------|");
+                }
+                default -> {
                     System.out.println("|----- ¡Opción No Válida!");
                     System.out.println("|-------------------------------------------------------------------|");
+                }
             }
             
         } while (menu != 0);
