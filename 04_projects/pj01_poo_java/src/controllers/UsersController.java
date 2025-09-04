8b732aa699bfc9279988768693da910c1dd87c47
ProfeAlbeiro <profealbeiro2020@gmail.com>
2025-09-04 17:24:56 -0500

    proyecto poo_java.txt

--- 04_projects/pj01_poo_java/src/controllers/UsersController.java
+++ 04_projects/pj01_poo_java/src/controllers/UsersController.java
@@ -10,13 +10,26 @@ public class UsersController {
     
     private int rolId, userId;
     private String userName, userLastName, userEmail, userPass;
+    private RolModel rol;
+    private UserModel user;
     private boolean userState;
     private UsersView usersView;
+    private UsersFormView usersFormView = new UsersFormView();
     private ArrayList<UserModel> users = new ArrayList<>();    
 
     public void usersMenu(RolesController roles, UsersController users) {        
         usersView = new UsersView(roles, users);
         usersView.usersMenuView();        
+    }
+    
+    public String addUser(){
+        userId = users.size() + 1;
+        userName = usersFormView.getUserName();
+        userLastName = usersFormView.getUserLastName();
+        userEmail = usersFormView.getUserEmail();
+        userPass = usersFormView.getUserPass();
+        users.add(user);
+        return "|----- ¡Usuario Creado!";
     }
     
 }
